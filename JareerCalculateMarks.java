0086ed87af2b58dd3aa78126359aa2a406398c58
Jareer <jareerahmedkhan@gmail.com>
2022-07-06 12:29:21 +0530

    example.txt

--- JareerCalculateMarks.java
+++ JareerCalculateMarks.java
@@ -1,11 +1,8 @@
-public class CalculateMarks {
+public DemoGit {
 
 public static void main(String args[]){
      
-	  Student jareerMarks = new Student();
-	  //Pass in the values in order first is emp Id, basic, hra, pf
-	  jareerMarks.calculateTotalMarks(06, 500, 50, 50, 50, 50);
-	  
+	  DemoGit = new Demogit ();
 	  
 	  
    }
