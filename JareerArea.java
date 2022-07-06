1cfe07c8c43cd1a3df3cb8b9a04f984cb0efcdf3
Jareer <jareerahmedkhan@gmail.com>
2022-07-06 11:59:55 +0530

    JareerArea.java

---
+++ JareerArea.java
@@ -0,0 +1,14 @@
+public class TestArea {
+
+     public satatic void main (String args []){
+
+	AreaofRectangle jareer = new AreaofRectangle () ; // creation of boject
+	anilboj .length=10 ;
+	anilobj .bresdth=20 ;
+
+	anilobj .calculateArea () ;
+
+     }
+
+
+}
